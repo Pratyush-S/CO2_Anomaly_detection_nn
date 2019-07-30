a5fdd79377b0aa0c495cf4e3ec3c100937a93839
Pratyush Shukla <shukla1024@gmail.com>
2019-07-30 16:40:35 +0530

    model training and passing to tick complete

--- CODE/Anomaly_detect/Scripts/threshold_based_labeling_for_c1_and_c3.m
+++ CODE/Anomaly_detect/Scripts/threshold_based_labeling_for_c1_and_c3.m
@@ -1,21 +1,36 @@
 
 th_0=310;%%1
 th_47=750;%%
-th_93=1150;%%
+th_93=1365;%%
 th_140=1565;%%
 th_186=1980;
+th_20=480;
+th_70=940;
+th_163=1850;
+th_170=1920;
 
 hm_0=0.0012612;
 hm_47=0.03943;
 hm_93=0.10000;
 hm_104=0.11548;
 hm_186=0.15176;
+hm_20=0.01751;
+hm_70=0.058074;
+hm_163=0.13323;
+hm_170=0.13887;
 
-%class_3=zeros(14400,1);
+
+
+class_3=zeros(14400,1);
 class_4=zeros(14400,1);
-%class_0=zeros(14400,1);
-%class_1=zeros(14400,1);
-%class_2=zeros(14400,1);
+class_0=zeros(14400,1);
+class_1=zeros(14400,1);
+class_2=zeros(14400,1);
+%humid_f=zeros(14401,1);
+%press_f=zeros(14401,1);
+%pass_f=zeros(14401,1);
+%temp_f=zeros(14401,1);
+
 
 z1=cat(1,300,CO2_Zone_1);
 z2=cat(1,300,CO2_Zone_2);
@@ -42,22 +57,46 @@ average_value=(CO2_Zone_1+CO2_Zone_2+CO2_Zone_3+CO2_Zone_4+CO2_Zone_5+CO2_Zone_6
 
 
 
+for i=1:14400
+    
+    if average_value(i)>=th_93
+    if average_slope(i)<0
+        class_4(i)=1;
+    elseif average_slope(i)>0
+        if humid_f(i)>hm_93
+            class_1(i)=1;
+        end
+    end
+    else
+        class_0(i)=1;
+    end
+end
 
-subplot(5,1,1)
-plot(average_value);
-subplot(5,1,2)
-plot(average_slope);
-subplot(5,1,3)
-plot(class_2);
-subplot(5,1,4)
-plot(class_4);
-subplot(5,1,5)
-plot(class_0);
 
+    subplot(5,1,1)
+    plot(average_value);
+    title("average value");
+    subplot(5,1,2)
+    plot(average_slope);
+    title("Average slope");
+    subplot(5,1,3)
+    plot(class_0);
+    title("Class 0");
+    subplot(5,1,4)
+    plot(class_1);
+    title("class 1");
+    subplot(5,1,5)
+    plot(class_4);
+    title("class_4");
+    
+    
+%final_array=cat(2,CO2_Zone_1,dz1,CO2_Zone_2,dz2,CO2_Zone_3,dz3,CO2_Zone_4,dz4,CO2_Zone_5,dz5,CO2_Zone_6,dz6,class_0,class_1,class_2,class_3,class_4);
 final_array=cat(2,CO2_Zone_1,dz1,CO2_Zone_2,dz2,CO2_Zone_3,dz3,CO2_Zone_4,dz4,CO2_Zone_5,dz5,CO2_Zone_6,dz6,temp_f,press_f,humid_f,pass_f,class_0,class_1,class_2,class_3,class_4);
 
+%final_table=array2table(final_array,'VariableNames',{'CO2_Zone_1','dz1','CO2_Zone_2','dz2','CO2_Zone_3','dz3','CO2_Zone_4','dz4','CO2_Zone_5','dz5','CO2_Zone_6','dz6','class_0','class_1','class_2','class_3','class_4'});
 final_table=array2table(final_array,'VariableNames',{'CO2_Zone_1','dz1','CO2_Zone_2','dz2','CO2_Zone_3','dz3','CO2_Zone_4','dz4','CO2_Zone_5','dz5','CO2_Zone_6','dz6','temp_f','press_f','humid_f','pass_f','class_0','class_1','class_2','class_3','class_4'});
 
-file_name=strcat('training_set_4hr_pascnt_',num2str(pass_f(4)),'.xlsx');
+file_name=strcat('training_set_4hr_pascnt_93_.xlsx');
+%file_name=strcat('training_set_4hr_pascnt_',num2str(20),'.xlsx');
 writetable(final_table,file_name);
 
--- CODE/Anomaly_detect/final_model_training.py
+++ CODE/Anomaly_detect/final_model_training.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.optimizers import SGD,Adam
-
 import os.path
 import json
 from keras.models import model_from_json
@@ -39,10 +37,11 @@ dataset12 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\tra
 dataset13 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\training dataset\class3_b\training_set_4hr_pascnt_47.xlsx')
 dataset14 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\training dataset\class3_b\training_set_4hr_pascnt_93.xlsx')
 dataset15 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\training dataset\class3_b\training_set_4hr_pascnt_140.xlsx')
-
+dataset16 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\training dataset\class3_b\training_set_4hr_pascnt_20.xlsx')
+dataset17 = pd.read_excel(r'D:\PS!\CO2_Anomaly_detection\CODE\Anomaly_detect\training dataset\class3_b\training_set_4hr_pascnt_70.xlsx')
 
 #Combine datasets into one single data file
-frames=[dataset1, dataset2, dataset3, dataset4, dataset5, dataset6, dataset7, dataset8, dataset9, dataset10, dataset11, dataset12, dataset13, dataset14, dataset15]
+frames=[dataset1, dataset2, dataset3, dataset4, dataset5, dataset6, dataset7, dataset8, dataset9, dataset10, dataset11, dataset12, dataset13, dataset14, dataset15, dataset16, dataset17]
 dataset = pd.concat(frames)
 
 #  Shuffle Data
@@ -55,15 +54,17 @@ dataset = dataset.sample(frac=1).reset_index(drop=True)
 X_complete=dataset.drop(['class_0','class_1','class_2','class_3','class_4'],axis=1)
 
 #y_complete=dataset['Class','zone_1']
-#y_complete=dataset[['class_0','class_1','class_2','class_3','class_4']]
+y_complete=dataset[['class_0','class_1','class_2','class_3','class_4']]
 
+#assigning different values to each output class
 y0=dataset['class_0']*1;
 y1=dataset['class_1']*2;
 y2=dataset['class_2']*3;
 y3=dataset['class_3']*4;
 y4=dataset['class_4']*5;
 
-y_complete=pd.concat([y0,y1,y2,y3,y4], axis=1, sort=False)
+#y_complete=pd.concat([y0,y1,y2,y3,y4], axis=1, sort=False)
+#y_complete=y1+y2+y0+y3+y4;
 
 print("Unnormalized Data", "\n", X_complete[:5], "\n")
 print("Unnormalized Data", "\n", y_complete[:5], "\n")
@@ -77,6 +78,10 @@ for i in col:
     X_complete[str(i)]=X_complete[str(i)].apply(lambda X:(X-avg)/(sd))
     print(avg)
     print(sd)
+    print(i)
+    
+    
+    
 print("Normalized Data\n", X_complete[:5], "\n")
 
 
@@ -84,17 +89,18 @@ print("Normalized Data\n", X_complete[:5], "\n")
 X_complete=X_complete.values
 
 #One hot encoding
-y_complete = pd.get_dummies(y_complete).values
+#_complete= pd.get_dummies(y_complete).values
+y_complete=y_complete.values
 
 # Creating a Train and a Test Dataset
-X_train, X_test, y_train, y_test = train_test_split(X_complete, y_complete, test_size=0.3, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(X_complete, y_complete, test_size=0.4, random_state=seed)
 
 
 # Define Neural Network model layers
 model = Sequential()
-model.add(Dense(12, input_dim=16, activation='relu'))
-model.add(Dense(12, activation='relu'))
-model.add(Dense(12, activation='relu'))
+model.add(Dense(20, input_dim=16, activation='softmax'))
+model.add(Dense(15, activation='softmax'))
+model.add(Dense(15, activation='softmax'))
 model.add(Dense(5, activation='softmax'))
 
 # Compile model
@@ -120,7 +126,7 @@ else:
     print("Model weights data not found. Model will be fit on training set now.")
 
     # Fit model on training data - try to replicate the normal input
-    model.fit(X_train,y_train,epochs=10,batch_size=100,verbose=1,validation_data=(X_test,y_test))
+    model.fit(X_train,y_train,epochs=30,batch_size=200,verbose=1,validation_data=(X_test,y_test))
     
     # Save parameters to JSON file
     model_json = model.to_json()
@@ -135,16 +141,16 @@ model.summary()
 
 
 
+
 # Model predictions for test set
 y_pred = model.predict(X_test)
 y_test_class = np.argmax(y_test,axis=1)
 y_pred_class = np.argmax(y_pred,axis=1)
 
-
 print(y_test_class,y_pred_class)
 
 # Evaluate model on test data
-score = model.evaluate(X_complete, y_complete, batch_size=128,verbose=1)
+score = model.evaluate(X_test, y_test, batch_size=128,verbose=1)
  
 # Compute stats on the test set and Output all results
 from sklearn.metrics import classification_report,confusion_matrix
