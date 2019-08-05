72176788557aba3802a13ba96181df8e5d44d050
Pratyush Shukla <shukla1024@gmail.com>
2019-08-05 12:41:01 +0530

    dataset class1 added

--- CODE/Anomaly_detect/Scripts/threshold_based_labeling_for_c1_and_c3.m
+++ CODE/Anomaly_detect/Scripts/threshold_based_labeling_for_c1_and_c3.m
@@ -1,12 +1,13 @@
-
+%x=14400;
+x=7200;
 th_0=310;%%1
 th_47=750;%%
 th_93=1365;%%
 th_140=1565;%%
 th_186=1980;
-th_20=480;
+th_20=490;
 th_70=940;
-th_163=1850;
+th_160=1850;
 th_170=1920;
 
 hm_0=0.0012612;
@@ -14,22 +15,35 @@ hm_47=0.03943;
 hm_93=0.10000;
 hm_140=0.11548;
 hm_186=0.15176;
-hm_20=0.01751;
+hm_20=0.01760;
 hm_70=0.058074;
 hm_163=0.13323;
+hm_160=0.13081;
 hm_170=0.13887;
 
 
 
-class_3=zeros(14400,1);
-class_4=zeros(14400,1);
-class_0=zeros(14400,1);
-class_1=zeros(14400,1);
-class_2=zeros(14400,1);
-humid_f=zeros(14400,1);
-press_f=zeros(14400,1);
-pass_f=zeros(14400,1);
-temp_f=zeros(14400,1);
+class_3=zeros(x,1);
+class_4=zeros(x,1);
+class_0=zeros(x,1);
+class_1=zeros(x,1);
+class_2=zeros(x,1);
+humid_f=zeros(x,1);
+press_f=zeros(x,1);
+pass_f=zeros(x,1);
+temp_f=zeros(x,1);
+
+class_3=zeros(x,1);
+class_4=zeros(x,1);
+class_0=zeros(x,1);
+class_1=zeros(x,1);
+class_2=zeros(x,1);
+humid_f=zeros(x,1);
+press_f=zeros(x,1);
+pass_f=zeros(x,1);
+temp_f=zeros(x,1);
+
+
 
 
 z1=cat(1,300,CO2_Zone_1);
@@ -38,12 +52,12 @@ z3=cat(1,300,CO2_Zone_3);
 z4=cat(1,300,CO2_Zone_4);
 z5=cat(1,300,CO2_Zone_5);
 z6=cat(1,300,CO2_Zone_6);
-z1(14401)=[];
-z2(14401)=[];
-z3(14401)=[];
-z4(14401)=[];
-z5(14401)=[];
-z6(14401)=[];
+z1(x)=[];
+z2(x)=[];
+z3(x)=[];
+z4(x)=[];
+z5(x)=[];
+z6(x)=[];
 
 dz1=CO2_Zone_1-z1;
 dz2=CO2_Zone_2-z2;
@@ -55,7 +69,7 @@ average_slope=(dz1+dz2+dz3+dz4+dz5+dz6)/6;
 average_value=(CO2_Zone_1+CO2_Zone_2+CO2_Zone_3+CO2_Zone_4+CO2_Zone_5+CO2_Zone_6)/6;
 
 k=0;
-for i = 1:240
+for i = 1:(x/60)
     for j = 1:60
         k=k+1;
         temp_f(k)=t_sim(i);
@@ -67,18 +81,15 @@ for i = 1:240
 end
 
 
-for i=1:14400
+for i=1:x
 
-if average_value(i)>=th_20
-    if humid_f(i)<=hm_20
-       if average_slope(i)<=0
+if humid_f(i)>hm_160
+   class_1(i)=1;
+elseif average_value(i)>th_160
+    if average_slope(i)<0
         class_4(i)=1;
-       end
-    else
-           class_1(i)=1;
     end
-    
-else
+else 
     class_0(i)=1;
     
 
@@ -114,7 +125,7 @@ final_array=cat(2,CO2_Zone_1,dz1,CO2_Zone_2,dz2,CO2_Zone_3,dz3,CO2_Zone_4,dz4,CO
 %final_table=array2table(final_array,'VariableNames',{'CO2_Zone_1','dz1','CO2_Zone_2','dz2','CO2_Zone_3','dz3','CO2_Zone_4','dz4','CO2_Zone_5','dz5','CO2_Zone_6','dz6','class_0','class_1','class_2','class_3','class_4'});
 final_table=array2table(final_array,'VariableNames',{'CO2_Zone_1','dz1','CO2_Zone_2','dz2','CO2_Zone_3','dz3','CO2_Zone_4','dz4','CO2_Zone_5','dz5','CO2_Zone_6','dz6','temp_f','press_f','humid_f','pass_f','class_0','class_1','class_2','class_3','class_4'});
 
-file_name=strcat('training_set_4hr_pascnt_20.xlsx');
+file_name=strcat('training_set_4hr_pascnt_160.xlsx');
 %file_name=strcat('training_set_4hr_pascnt_',num2str(20),'.xlsx');
 writetable(final_table,file_name);
 
